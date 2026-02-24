2a96efd4af2392df3e3d46a4f2e6db990fa8575c
Sravanthi <23MH1A05E5@acoe.edu.in>
2026-02-24 11:19:51 +0530

    initial commit

---
+++ src/main/java/com/example/demo/Csecontroller.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+
+public class Csecontroller {
+
+	@GetMapping("/home")
+	public String rocks() {
+		return "Welcome to Jekins AWS";
+	}
+	@GetMapping("/cse")
+		public String rocks1() {
+			return "Welcome to CSE";
+		}
+	}
